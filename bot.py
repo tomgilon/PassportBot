f1e871f53b90353e2416e26eadb3d8bdd124c665
Tom Gilon <tomgilon@gmail.com>
2022-06-20 01:58:49 +0300

    remove unrelated crap

--- bot.py
+++ bot.py
@@ -2,26 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import json
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler, CallbackQueryHandler
-from telegram import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+import json 
 from telegram.ext.updater import Updater
 from telegram.update import Update
-from telegram.ext.callbackcontext import CallbackContext
-from telegram.ext.commandhandler import CommandHandler
-from telegram.ext.messagehandler import MessageHandler
-from telegram.ext.filters import Filters
-from functools import wraps
-from collections import OrderedDict
 import locale
 import time
 import _thread as thread
 import requests
-import re
-import os
-import pickle
-
-import datetime
+from datetime import datetime
+import pytz
 
 user_config = {}
 
@@ -31,24 +20,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-def restricted(func):
-    @wraps(func)
-    def wrapped(update, context, *args, **kwargs):
-        if update.message is not None:
-            chat_id = update.message.chat.id
-        else:
-            chat_id = update.callback_query.message.chat.id
-        if str(chat_id) != user_config['telegram_chat_id']:
-            print('Unauthorized access denied for chat {}.'.format(chat_id))
-            context.bot.send_message(chat_id=user_config['telegram_chat_id'], text='Unauthorized access denied for chat {}.'.format(chat_id))
-            return
-        return func(update, context, *args, **kwargs)
-    return wrapped
-
 def find_appointments(update):
-    import requests
-    from datetime import datetime
-    import pytz
     update.bot.send_message(chat_id=user_config['telegram_chat_id'], text="Starting curl loop")
     while True:
         date = datetime.now(pytz.timezone("israel")).strftime("%Y-%m-%d")
@@ -85,14 +57,6 @@ def find_appointments(update):
         time.sleep(20)
 
 
-@restricted
-def unknown_command(updater, context):
-    updater.message.reply_text(text='לא הבנתי...', reply_markup=reply_markup)
-
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-
 def initialize_user_config(path='config.json'):
     global user_config
     
@@ -100,19 +64,7 @@ def initialize_user_config(path='config.json'):
         data = f.read()
     
     user_config = json.loads(data)
-    """
-    Todo: implement cookies cache file
-    
-    global cookies_cache
-    if os.path.exists(cookie_cache_path):
-        with open(cookie_cache_path, 'rb') as f:
-            cookies_cache = pickle.load(f)
-    """
 
-@restricted
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "totaly, {}".format(update.message.chat.id))
 
 def main():
     # load user configuration file
@@ -125,22 +77,12 @@ def main():
     # Post version 12 this will no longer be necessary
     updater = Updater(user_config['telegram_api_key'], use_context=True)
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # log all errors
-    dp.add_error_handler(error)
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-
-    #updater.bot.send_message(chat_id=user_config['telegram_chat_id'], text='מה תרצה לעשות?', reply_markup=reply_markup)
-
     # Start the Bot
     updater.start_polling()
     
-    # start new thread for daily notifications
+    # start new thread for new appointment checks
     args = (updater,)
     thread.start_new_thread(find_appointments, args)
-    #thread.start_new_thread(setup_one_identity_routine, args)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
